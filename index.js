a329d98d302582b56eea84bb6d1ef480a8f83d73
celine.roure <celine.roure.pro@gmail.com>
2021-11-23 11:13:12 +1100

    hello world works

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// Initialize Express as an instance named "app".
+const app = express();
+
+// Separate these out in case we wanna use Docker or something to wrap the app.
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+// we can receive JSON data from POST/PUT/PATCH/etc requests
+app.use(express.json());
+
+// for form data
+app.use(express.urlencoded({extended:true}));
+
+// Standard ExpressJS route, sends back a HTML response
+app.get('/', (request, response) => {
+  response.json("It's Tuesday yeah !");
+});
+
+app.listen(PORT, HOST, () => {
+  console.log("Server is running!")
+});
